89a49fd2f21d7d64df370e4291f177409bb71d8e
Lovro Mažgon <lovro.mazgon@gmail.com>
2024-10-18 18:55:12 +0200

    make generate

--- destination/mock/destination.go
+++ destination/mock/destination.go
@@ -21,6 +21,7 @@ import (
 type MockPublisher struct {
 	ctrl     *gomock.Controller
 	recorder *MockPublisherMockRecorder
+	isgomock struct{}
 }
 
 // MockPublisherMockRecorder is the mock recorder for MockPublisher.
